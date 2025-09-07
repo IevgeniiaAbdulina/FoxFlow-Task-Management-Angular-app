31b0382b3abe51adb3992c847f0438e4dd0aeba8
ina <ina.flaryanovich@gmail.com>
2025-09-07 19:30:19 +0200

    feat: add initial i18n support and languages switcher

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,14 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  HttpClient,
+} from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import {
@@ -18,11 +24,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { environment } from 'src/environments/environment.development';
 
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
@@ -30,5 +41,13 @@ export const appConfig: ApplicationConfig = {
     UserTrackingService,
     provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase()),
+    ...TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }).providers!,
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import {
 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseServiceTs } from './services/firebase/firebase-service';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,11 +21,27 @@ export class App implements OnInit {
   protected readonly testConnection = signal('');
 
   firebaseService = inject(FirebaseServiceTs);
+  protected translate = inject(TranslateService);
 
   ngOnInit(): void {
+    this.translate.addLangs(['en', 'pl']);
+    this.translate.setDefaultLang('en');
+
+    const savedLang = localStorage.getItem('lang');
+    const browserLang = this.translate.getBrowserLang();
+
+    this.translate.use(
+      savedLang || (browserLang?.match(/en|pl/) ? browserLang : 'en')
+    );
+
     this.firebaseService.getTestConnection().subscribe((documents) => {
       const firstDoc = documents[0];
       this.testConnection.set(firstDoc?.['text'] ?? '');
     });
+  }
+
+  switchLanguage(lang: string): void {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
   }
 }
