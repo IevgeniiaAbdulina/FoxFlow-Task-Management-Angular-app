66a3dc40680a37c798617724e2d7ab2378455307
Olga Rekun <rekun.olya@gmail.com>
2025-09-09 23:55:40 +0200

    feat: create main component and add project description

---
+++ src/app/shared/components/main/main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [TranslateModule],
+  templateUrl: './main.html',
+  styleUrl: './main.scss',
+})
+export class Main {}
