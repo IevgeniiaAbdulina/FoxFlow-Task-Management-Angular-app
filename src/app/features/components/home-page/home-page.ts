1953d722903ffd03476df707f5954e4868de370c
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-09-15 12:22:55 +0200

    fix: show user info, update tsconfig file.

--- src/app/features/components/home-page/home-page.ts
+++ src/app/features/components/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '@app/auth/services/auth-service';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [AsyncPipe, NgOptimizedImage],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
--- src/app/features/components/main-page/main-page.ts
+++ src/app/features/components/main-page/main-page.ts
@@ -1,20 +1,16 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { LanguageSwitcherComponent } from '@app/shared/components/language-switcher/language-switcher.component';
 import { MatCard } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { AuthService } from '@app/auth/services/auth-service';
 import { FirebaseServiceTs } from '@app/services/firebase/firebase-service';
 import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-page',
   imports: [
-    AsyncPipe,
     LanguageSwitcherComponent,
     MatCard,
-    NgOptimizedImage,
     RouterLink,
     TranslateModule,
     MatButton,
@@ -24,7 +20,6 @@ import { MatButton } from '@angular/material/button';
 })
 export class MainPage implements OnInit {
   firebaseService = inject(FirebaseServiceTs);
-  authService = inject(AuthService);
 
   protected readonly testConnection = signal('');
 
