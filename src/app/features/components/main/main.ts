c32f32c067a7003bb4871fbca7dcc81078eb44dd
Olga Rekun <rekun.olya@gmail.com>
2025-09-15 13:41:39 +0200

    fix: fix loading owners section

--- src/app/features/components/main/main.ts
+++ src/app/features/components/main/main.ts
@@ -3,14 +3,14 @@ import {
   Component,
   inject,
   OnInit,
-  output,
+  signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { Owner } from '../../../shared/components/owner/owner';
 import { OwnerData } from '@app/shared/interfaces/owner-interface';
-import { OwnersService } from './owners-service/owners-service';
+import { OwnersService } from '../../services/owners-service/owners-service';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -28,13 +28,13 @@ import { MatButtonModule } from '@angular/material/button';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Main implements OnInit {
-  owners: OwnerData[] = [];
+  readonly owners = signal<OwnerData[]>([]);
 
   ownersService = inject(OwnersService);
 
   ngOnInit(): void {
     this.ownersService.getOwners().subscribe((owners) => {
-      this.owners = owners;
+      this.owners.set(owners);
     });
   }
 
