c3f51ef3eb39eb62db673bc623e050fa6598504a
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-09-18 17:57:20 +0200

    feat: Add Projects list on the Home page.

--- src/app/features/components/home-page/home-page.ts
+++ src/app/features/components/home-page/home-page.ts
@@ -1,22 +1,40 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  computed,
+  Signal,
+} from '@angular/core';
 import { AuthService } from '@app/auth/services/auth-service';
-import { NgOptimizedImage } from '@angular/common';
+import { GreetingComponent } from '@app/features/components/greeting-component/greeting-component';
+import { UserInterface } from '@app/shared/interfaces/user-interface';
+import { ProjectsFirebaseService } from '@app/features/services/projects-service/projects-firebase-service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NgOptimizedImage],
+  imports: [
+    GreetingComponent,
+    MatTooltipModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
   private authService = inject(AuthService);
-  readonly user$ = toSignal(this.authService.user$, {
-    initialValue: null,
+  private projectsFirebaseService = inject(ProjectsFirebaseService);
+
+  readonly projects = toSignal(this.projectsFirebaseService.getProjects(), {
+    initialValue: [],
   });
 
-  logout(): void {
-    this.authService.logout();
-  }
+  readonly user$ = computed(() =>
+    this.authService.currentUser()
+  ) as Signal<UserInterface>;
 }
---
+++ src/app/shared/components/projects-list-large/projects-list-large.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Project } from '@app/shared/interfaces/project-interface';
+import { ProjectsListItem } from '@app/shared/components/projects-list-item/projects-list-item';
+
+@Component({
+  selector: 'app-projects-list-large',
+  imports: [ProjectsListItem],
+  templateUrl: './projects-list-large.html',
+  styleUrl: './projects-list-large.scss',
+})
+export class ProjectsListLarge {
+  readonly projects$ = input.required<Project[]>();
+}
