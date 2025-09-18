1b935e6edd0a2f5ef8bf147cc4e2cf82e084d1ab
Olga Rekun <rekun.olya@gmail.com>
2025-09-18 15:54:04 +0200

    fix: update firebaseService

--- src/app/services/firebase/firebase-service.ts
+++ src/app/services/firebase/firebase-service.ts
@@ -5,8 +5,13 @@ import {
   collection,
   DocumentData,
   collectionData,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { TaskData } from '@app/shared/interfaces/task-interface';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +24,46 @@ export class FirebaseServiceTs {
     return collectionData(this.testCollection, {
       idField: 'id',
     });
+  }
+
+  getProjectTasks(projectId: string): Observable<TaskData[]> {
+    const projectDocRef = doc(this.firestore, `projects/${projectId}`);
+    const taskCollectionRef = collection(projectDocRef, 'task-project1');
+    return collectionData(taskCollectionRef, { idField: 'id' }) as Observable<
+      TaskData[]
+    >;
+  }
+
+  addTask(text: string, projectId: string): Observable<string> {
+    const projectDocRef = doc(this.firestore, `projects/${projectId}`);
+    const taskCollectionRef = collection(projectDocRef, 'task-project1');
+    const todoToCreate = { title: text, isCompleted: false };
+
+    const promise = addDoc(taskCollectionRef, todoToCreate).then(
+      (response) => response.id
+    );
+    return from(promise);
+  }
+
+  deleteTask(projectId: string, taskId: string): Observable<void> {
+    const docRef = doc(
+      this.firestore,
+      `projects/${projectId}/task-project1/${taskId}`
+    );
+    const promise = deleteDoc(docRef);
+    return from(promise);
+  }
+
+  updateTask(
+    projectId: string,
+    taskId: string,
+    updateFields: Partial<TaskData>
+  ): Observable<void> {
+    const docRef = doc(
+      this.firestore,
+      `projects/${projectId}/task-project1/${taskId}`
+    );
+    const promise = updateDoc(docRef, updateFields);
+    return from(promise);
   }
 }
