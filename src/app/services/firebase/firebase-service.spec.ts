164a4e44a8e69ddf93ff02af8667165fabfe3e90
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 17:16:24 +0200

    fix: fix imports for test on firebase service

--- src/app/services/firebase/firebase-service.spec.ts
+++ src/app/services/firebase/firebase-service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FirebaseServiceTs } from './firebase-service';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 describe('FirebaseServiceTs', () => {
   let service: FirebaseServiceTs;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        FirebaseServiceTs,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideFirestore(() => getFirestore()),
+      ],
+    });
     service = TestBed.inject(FirebaseServiceTs);
   });
 
