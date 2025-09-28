bf87f22a79eba1dc420758465c9bd5d6782575af
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 12:45:51 +0200

    fix: fix imports for test greeting  component

--- src/app/features/components/greeting-component/greeting-component.spec.ts
+++ src/app/features/components/greeting-component/greeting-component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GreetingComponent } from './greeting-component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('GreetingComponent', () => {
   let component: GreetingComponent;
@@ -8,11 +9,15 @@ describe('GreetingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GreetingComponent],
+      imports: [GreetingComponent, TranslateModule.forRoot()],
+      providers: [TranslateService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GreetingComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('user$', { name: 'Test User', id: 1 });
+    fixture.componentRef.setInput('projects$', []);
+
     fixture.detectChanges();
   });
 
