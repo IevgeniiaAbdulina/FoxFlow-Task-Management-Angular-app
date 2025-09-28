04c5b818f89fe5e753932657c504eb9e426abef3
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 17:24:37 +0200

    fix: fix imports for test on project  service

--- src/app/features/services/projects-service/project-service.spec.ts
+++ src/app/features/services/projects-service/project-service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectService } from './project-service';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import { Auth } from '@angular/fire/auth';
 
 describe('ProjectService', () => {
   let service: ProjectService;
+  const mockAuth = jasmine.createSpyObj('Auth', [
+    'signInWithEmailAndPassword',
+    'signOut',
+  ]);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ProjectService,
+        { provide: Auth, useValue: mockAuth },
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideFirestore(() => getFirestore()),
+      ],
+    });
     service = TestBed.inject(ProjectService);
   });
 
