4acf019f6e59409162c160cb9ac5cec8dd95212a
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 19:09:05 +0200

    fix: fix imports for test app component

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
+import { Auth } from '@angular/fire/auth';
+import { FirebaseServiceTs } from '@app/services/firebase/firebase-service';
+import { AuthService } from './auth/services/auth-service';
+import { provideRouter } from '@angular/router';
+import { routes } from '@app/app.routes';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+
 describe('App', () => {
+  const mockAuth = jasmine.createSpyObj('Auth', [
+    'signInWithEmailAndPassword',
+    'signOut',
+  ]);
+
+  let mockFirebaseService: jasmine.SpyObj<FirebaseServiceTs>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+
   beforeEach(async () => {
+    const mockUser = {
+      uid: '123',
+      email: 'test@example.com',
+      displayName: 'Test User',
+      photoURL: 'http://example.com/photo.jpg',
+    };
+    mockFirebaseService = jasmine.createSpyObj('FirebaseServiceTs', ['']);
+    mockAuthService = jasmine.createSpyObj('AuthService', ['currentUser'], {
+      user$: of(mockUser), // или of(mockUser) для имитации авторизованного пользователя
+    });
+
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, TranslateModule.forRoot()],
+      providers: [
+        TranslateService,
+        provideRouter(routes),
+        { provide: Auth, useValue: mockAuth },
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: FirebaseServiceTs, useValue: mockFirebaseService },
+      ],
     }).compileComponents();
   });
 
