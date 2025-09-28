0c4df836078e7699591c247cc31e63b7ed735207
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 19:14:58 +0200

    fix: fix imports for test project details component

--- src/app/shared/components/project-details-dialog/project-details-dialog.spec.ts
+++ src/app/shared/components/project-details-dialog/project-details-dialog.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectDetailsDialog } from './project-details-dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('ProjectDetailsDialog', () => {
   let component: ProjectDetailsDialog;
@@ -9,6 +10,16 @@ describe('ProjectDetailsDialog', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProjectDetailsDialog],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: { close: jasmine.createSpy('close') },
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { name: 'Test Project' },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectDetailsDialog);
