4bf9e2d51d228072bff09cef564b92fcb99b3cad
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 12:47:12 +0200

    fix: fix imports for test task-header  component

--- src/app/shared/components/task-header/task-header.spec.ts
+++ src/app/shared/components/task-header/task-header.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskHeader } from './task-header';
+import { FirebaseServiceTs } from '@app/services/firebase/firebase-service';
+import { ProjectService } from '@app/features/services/projects-service/project-service';
+import { of } from 'rxjs';
+//import { Auth } from '@angular/fire/auth';
+//import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+//import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('TaskHeader', () => {
   let component: TaskHeader;
   let fixture: ComponentFixture<TaskHeader>;
+  let mockFirebaseService: jasmine.SpyObj<FirebaseServiceTs>;
+  let mockProjectService: jasmine.SpyObj<ProjectService>;
 
   beforeEach(async () => {
+    mockProjectService = jasmine.createSpyObj('ProjectService', [
+      'currentProject',
+    ]);
+    mockFirebaseService = jasmine.createSpyObj('FirebaseServiceTs', [
+      'addTask',
+    ]);
+    //const mockAuth = jasmine.createSpyObj('Auth', ['signInWithEmailAndPassword', 'signOut']);
+
     await TestBed.configureTestingModule({
-      imports: [TaskHeader],
+      imports: [TaskHeader, TranslateModule.forRoot()],
+      providers: [
+        TranslateService,
+        //provideFirebaseApp(() => initializeApp(environment.firebase)),
+        //provideFirestore(() => getFirestore()),
+        //{provide: Auth, useValue: mockAuth },
+        { provide: ProjectService, useValue: mockProjectService },
+        { provide: FirebaseServiceTs, useValue: mockFirebaseService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TaskHeader);
@@ -18,5 +43,26 @@ describe('TaskHeader', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('shoud add task and reset text', () => {
+    const mockProjectId = 'projectId';
+    mockProjectService.currentProject.and.returnValue({
+      id: mockProjectId,
+      title: '',
+      owner: '',
+      createdAt: '',
+    });
+    mockFirebaseService.addTask.and.returnValue(of('mock-task-id'));
+
+    component.text = 'test';
+    component.addTask();
+
+    expect(mockFirebaseService.addTask).toHaveBeenCalledOnceWith(
+      'test',
+      mockProjectId,
+      jasmine.any(Date)
+    );
+    expect(component.text).toBe('');
   });
 });
