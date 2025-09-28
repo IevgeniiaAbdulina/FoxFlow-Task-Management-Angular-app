f0f6e15afd7795870cec70c4745ee022e3c42d58
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 14:56:02 +0200

    fix: fix imports for test project-header component

--- src/app/features/components/project-header/project-header.spec.ts
+++ src/app/features/components/project-header/project-header.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectHeader } from './project-header';
+import { ProjectService } from '@app/features/services/projects-service/project-service';
+import { provideRouter } from '@angular/router';
+import { routes } from '@app/app.routes';
 
 describe('ProjectHeader', () => {
   let component: ProjectHeader;
   let fixture: ComponentFixture<ProjectHeader>;
 
+  let mockProjectService: jasmine.SpyObj<ProjectService>;
+
   beforeEach(async () => {
+    mockProjectService = jasmine.createSpyObj('ProjectService', [
+      'currentProject',
+    ]);
+
     await TestBed.configureTestingModule({
       imports: [ProjectHeader],
+      providers: [
+        provideRouter(routes),
+        { provide: ProjectService, useValue: mockProjectService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectHeader);
