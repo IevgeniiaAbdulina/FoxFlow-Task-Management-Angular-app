1148b5cdd79ca719814763c7b3848490c0007099
Olga Rekun <rekun.olya@gmail.com>
2025-09-28 14:48:39 +0200

    fix: fix imports for test project-page component

--- src/app/features/components/project-page/project-page.spec.ts
+++ src/app/features/components/project-page/project-page.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectPage } from './project-page';
+import { FirebaseServiceTs } from '@app/services/firebase/firebase-service';
+import { ProjectService } from '@app/features/services/projects-service/project-service';
+import { provideRouter } from '@angular/router';
+import { routes } from '@app/app.routes';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('ProjectPage', () => {
   let component: ProjectPage;
   let fixture: ComponentFixture<ProjectPage>;
 
+  let mockFirebaseService: jasmine.SpyObj<FirebaseServiceTs>;
+  let mockProjectService: jasmine.SpyObj<ProjectService>;
+
   beforeEach(async () => {
+    mockFirebaseService = jasmine.createSpyObj('FirebaseServiceTs', ['']);
+    mockProjectService = jasmine.createSpyObj('ProjectService', [
+      'currentProject',
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [ProjectPage],
+      imports: [ProjectPage, TranslateModule.forRoot()],
+      providers: [
+        provideRouter(routes),
+        TranslateService,
+        { provide: ProjectService, useValue: mockProjectService },
+        { provide: FirebaseServiceTs, useValue: mockFirebaseService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectPage);
